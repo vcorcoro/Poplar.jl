5c4db30b1a984add2f4bca16b09dcb7cea31bbb2
vcorcoro <137559813+vcorcoro@users.noreply.github.com>
2026-02-19 15:51:39 -0800

    uncomment fAge

--- src/phenology/age.jl
+++ src/phenology/age.jl
@@ -21,26 +21,26 @@
 
     # CODE BELOW OBSOLETE AFTER INCLUSION OF CARBON PARTITIONING TABLE
 
-    # "Maximum physiological age (check fAge for equation)"
-    # maxAge => 50 ~ preserve(parameter)
+    "Maximum physiological age (check fAge for equation)"
+    maxAge => 50 ~ preserve(parameter)
     
-    # "Power of relative age in function for fAge (check fAge for equation)"
-    # nAge => 4 ~ preserve(parameter)
+    "Power of relative age in function for fAge (check fAge for equation)"
+    nAge => 4 ~ preserve(parameter)
     
-    # "Relative age to give fAge = 0.5 (check fAge for equation)"
-    # rAge => 0.95 ~ preserve(parameter)
+    "Relative age to give fAge = 0.5 (check fAge for equation)"
+    rAge => 0.95 ~ preserve(parameter)
     
     #==
     Age
     ==#
 
-    # # flag to check if nAge is 0. If nAge is 0, fAge is 1.
-    # flagAge(nAge) => nAge != 0 ~ flag
+    # flag to check if nAge is 0. If nAge is 0, fAge is 1.
+    flagAge(nAge) => nAge != 0 ~ flag
 
-    # Empirical age-based modifier for physiology.
-    # Used in another modifier variable, fPhysiology.
-    # "Age-based physiological modifier"
-    # fAge(standAge, maxAge, rAge, nAge) => begin
-    #     (1 / (1 + (standAge / maxAge / rAge) ^ nAge)) 
-    # end ~ track(when=flagAge, init=1)
+    Empirical age-based modifier for physiology.
+    Used in another modifier variable, fPhysiology.
+    "Age-based physiological modifier"
+    fAge(standAge, maxAge, rAge, nAge) => begin
+        (1 / (1 + (standAge / maxAge / rAge) ^ nAge)) 
+    end ~ track(when=flagAge, init=1)
 end
